89f10181d5e261a9ecb80d5dd3699dd7610d738e
Owen Lee <hwolee@connect.ust.hk>
2021-12-22 17:53:22 +0800

    Completed UI

--- src/components/task-item/DoneItem.tsx
+++ src/components/task-item/DoneItem.tsx
@@ -9,14 +9,13 @@ type taskitem_props = {
   isEditing: any;
   editItem: any;
   deleteItem: any;
-  finishItem: any;
 };
 
-export default function TaskItem(props: taskitem_props) {
+export default function DoneItem(props: taskitem_props) {
 
   const [Name, setName] = useState<string>(props.name);
   const [SeqNum, setSeqNum] = useState<number>(props.seq_num);
-  const [Edit, setEdit] = useState<boolean>(true);
+  const [Edit, setEdit] = useState<boolean>(false);
 
   function verifyInput() {
     // overall editing and this editing
@@ -46,21 +45,15 @@ export default function TaskItem(props: taskitem_props) {
 
   }
 
-  function finish() {
-    props.finishItem(props.id);
-  }
-
   function deleteItem() {
-    if (window.confirm(`Delete "${Name}"?`)) props.deleteItem(props.id);
+    props.deleteItem(props.id);
   }
 
   return (
     <div id={props.id} style={{height: 50, width: '90%', backgroundColor: '#EEEEEE', margin: 5, display: "flex", flexDirection: "row", borderRadius: 8, alignItems: "center",}} >
       <p style={{height: 0, width: 0, margin: 0, fontSize: 0,}}>{props.id}</p>
       <input style={{fontSize: 25, marginLeft: 7, width: 50}} type='number' min={0} step={1} value={SeqNum} disabled={!Edit} onChange={(e => {setSeqNum(parseInt(e.target.value))})} />
-      <input style={{fontSize: 20, marginLeft: 7, flexGrow: 1, flexShrink: 1,}} value={Name} placeholder="Enter name of task" disabled={!Edit} onChange={e => {setName(e.target.value);}} />
-      <button style={{height: 40, width: 50, fontSize: 18, marginLeft: 7}} onClick={e => {verifyInput();}}>Edit</button>
-      <button style={{height: 40, marginLeft: 7, width: 70, fontSize: 18}} disabled={Edit} onClick={e => {finish();}}>Finish</button>
+      <input style={{fontSize: 20, marginLeft: 7, flexGrow: 1, flexShrink: 1,}} value={Name} disabled={!Edit} onChange={e => {setName(e.target.value);}} />
       <button style={{height: 40, marginLeft: 7, marginRight: 7, width: 70, fontSize: 18}} disabled={Edit} onClick={e => {deleteItem();}}>Delete</button>
     </div>
   );
--- src/components/task-list/TaskList.tsx
+++ src/components/task-list/TaskList.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TaskItem from "../task-item/TaskItem";
+import DoneItem from "../task-item/DoneItem";
 
 export default function TaskList() {
 
@@ -7,10 +8,12 @@ export default function TaskList() {
     id: string;
     name: string;
     seq_num: number;
+    done: boolean;
   }
 
   const [List, setList] = useState<IListItem[]>([]);
   const [Sequence, setSequence] = useState<string[]>([]);
+  const [DoneSequence, setDoneSequence] = useState<string[]>([]);
   const [EditingItem, setEditingItem] = useState(false);
 
   function addItem(name: string, seq_num: number) {
@@ -20,6 +23,7 @@ export default function TaskList() {
       id: id,
       name: name,
       seq_num: seq_num,
+      done: false,
     };
     setSequence([...Sequence, newitem.id]);
     setList([...List, newitem]);
@@ -35,6 +39,7 @@ export default function TaskList() {
       id: id,
       name: name,
       seq_num: seq_num,
+      done: List[idx].done,
     };
     newlist[idx] = newitem;
     setList([...newlist]);
@@ -46,6 +51,27 @@ export default function TaskList() {
   function deleteItem(id: string) {
     setList([...List.filter(item => item.id !== id)]);
     setSequence([...Sequence.filter(item => item !== id)]);
+    setDoneSequence([...DoneSequence.filter(item => item !== id)]);
+  }
+
+  function finishItem(id: string) {
+
+    function sendEmail() {
+
+    }
+
+    const finished: string = Sequence.find((item: string) => item === id) as string;
+    setDoneSequence([...DoneSequence, finished]);
+
+    const newsequence = Sequence.filter(item => item !== id);
+    setSequence([...newsequence]);
+
+    const idx = List.findIndex(item => (id === item.id));
+    const newlist = List;
+    newlist[idx].done = true;
+    setList([...newlist]);
+
+    sendEmail();
   }
 
   function sortItems() {
@@ -56,28 +82,61 @@ export default function TaskList() {
       const item_b: IListItem = List.find(item => item.id === b) as IListItem;
       return item_a.seq_num - item_b.seq_num;
     })])
-    setList(Sequence.map(id => List.find(item => item.id === id) as IListItem));
+    // setList(Sequence.map(id => List.find(item => item.id === id) as IListItem));
     // setList((nl) => [...nl.sort((a: IListItem, b: IListItem) => {return (a.seq_num - b.seq_num)})]);
     // setSequence(List.map(item => item.id));
     return;
   }
 
+  function clearHistory() {
+    if (window.confirm("Clear all task history?")) {
+      setList([...List.filter(item => DoneSequence.includes(item.id))]);
+      setDoneSequence([]);
+    }
+  }
+
   return (
-    <div style={{width: '100%'}} >
-      <div>
-        <button style={{textAlign: 'right',}} disabled={Sequence.length===0 || EditingItem} onClick={e => {sortItems();}}>Sort</button>
+    <div style={{width: '100%', display: 'flex', flexDirection: 'row'}}>
+      <div style={{width: '55%'}} >
+        <h2>To-do list</h2>
+        <div>
+          <button style={{textAlign: 'right',}} disabled={Sequence.length===0 || EditingItem} onClick={e => {sortItems();}}>Sort</button>
+        </div>
+        <div style={{marginTop: 20, display: "flex", flexDirection: 'column', alignItems: "center"}} >
+            {
+                [...Sequence].map((id) => {
+                    const taskitem: IListItem = List.find(item => item.id === id) as IListItem;
+                    return (
+                        <TaskItem name={taskitem.name} seq_num={taskitem.seq_num} id={id} key={id} EditOutside={EditingItem} isEditing={setEditingItem} editItem={editItem} deleteItem={deleteItem} finishItem={finishItem} />
+                        );
+                    })
+                }
+        </div>
+        <button style={{width: '40%', height: 40, marginTop: 20, fontSize: 30,}} disabled={EditingItem} 
+            onClick={e => {
+                addItem("", (Sequence.length ? Math.max(...Sequence.map(id => {
+                    const item: IListItem = List.find((item: IListItem) => item.id === id) as IListItem;
+                    return item.seq_num;
+                }))+1 : 1));
+            }}
+        >+</button>
       </div>
-      <div style={{marginTop: 20, display: "flex", flexDirection: 'column', alignItems: "center"}} >
-        {
-          [...Sequence].map((id) => {
-            const taskitem: IListItem = List.find(item => item.id === id) as IListItem;
-            return (
-              <TaskItem name={taskitem.name} seq_num={taskitem.seq_num} id={id} key={id} EditOutside={EditingItem} isEditing={setEditingItem} editItem={editItem} deleteItem={deleteItem} />
-            );
-          })
-        }
+      <div style={{width: '45%'}} >
+        <h2>Completed tasks</h2>
+        <div>
+          <button style={{textAlign: 'right',}} disabled={DoneSequence.length===0} onClick={e => {clearHistory();}}>Clear history</button>
+        </div>
+        <div style={{marginTop: 20, display: "flex", flexDirection: 'column', alignItems: "center"}} >
+            {
+                [...DoneSequence].map((id) => {
+                    const taskitem: IListItem = List.find(item => item.id === id) as IListItem;
+                    return (
+                        <DoneItem name={taskitem.name} seq_num={taskitem.seq_num} id={id} key={id} EditOutside={EditingItem} isEditing={setEditingItem} editItem={editItem} deleteItem={deleteItem} />
+                        );
+                    })
+                }
+        </div>
       </div>
-      <button style={{width: '40%', height: 40, marginTop: 20, fontSize: 30,}} disabled={EditingItem} onClick={e => {addItem("Todo", (List.length ? Math.max(...List.map(item => item.seq_num))+1 : 1));}}>+</button>
     </div>
   );
 }
